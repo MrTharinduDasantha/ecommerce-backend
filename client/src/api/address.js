9c09599a3e3f7c605cadc99cf3f94de84a00763f
Udara Lakshan <ulakshan061@gmail.com>
2025-06-09 10:40:17 +0530

    feat: enhance address management with delete and update functionality

--- client/src/api/address.js
+++ client/src/api/address.js
@@ -42,3 +42,45 @@ export const getAddressByCustomerId = async customerId => {
     throw new Error(error.response?.data?.message || "Failed to get address")
   }
 }
+
+export const deleteAddressById = async (customerId, addressId) => {
+  try {
+    const token = getToken()
+    const res = await fetch(
+      `http://localhost:9000/api/customer-addresses/${customerId}/addresses/${addressId}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+    if (!res.ok) throw new Error(`Error: ${res.status}`)
+    return "Address deleted successfully"
+  } catch (error) {
+    console.error("Error deleting customer address: ", error)
+    throw new Error(error.response?.data?.message || "Failed to delete address")
+  }
+}
+
+export const updateAddress = async (customerId, addressId, updatedAddress) => {
+  try {
+    const token = getToken()
+    const res = await fetch(
+      `http://localhost:9000/api/customer-addresses/${customerId}/addresses/${addressId}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(updatedAddress),
+      }
+    )
+    if (!res.ok) throw new Error(`Error: ${res.status}`)
+    return "Address updated successfully"
+  } catch (error) {
+    console.error("Error updating customer address: ", error)
+    throw new Error(error.response?.data?.message || "Failed to update address")
+  }
+}
