a9d1a5e0a1eca4bc89843fcb8d1e668a6fbcc7ab
Udara Lakshan <ulakshan061@gmail.com>
2025-07-03 23:23:20 +0530

    refactor review routes: streamline authentication usage for order and product reviews

--- src/routes/customer/review.routes.js
+++ src/routes/customer/review.routes.js
@@ -4,8 +4,19 @@ const reviewController = require("../../controllers/customer/review.controller")
 
 const router = express.Router()
 
-router.use(authenticateToken)
-
-router.post("/", reviewController.submitReview)
+router.post("/product", authenticateToken, reviewController.submitProductReview)
+router.post("/order", authenticateToken, reviewController.submitOrderReview)
+router.get("/product/:id", reviewController.getReviewsByProductId)
+router.get(
+  "/order/:id",
+  authenticateToken,
+  reviewController.getReviewsByOrderId
+)
+router.put("/order/:id", authenticateToken, reviewController.updateOrderReview)
+router.delete(
+  "/order/:id",
+  authenticateToken,
+  reviewController.deleteOrderReview
+)
 
 module.exports = router
