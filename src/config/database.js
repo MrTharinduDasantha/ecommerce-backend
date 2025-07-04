6e8a99e4e1a45ddc0c55757f871d3c1db1345407
rashel7r <rashel7r@gmail.com>
2025-07-04 17:49:43 +0530

    added some price updates

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root",
+  password: process.env.DB_PASSWORD || "NewStrongPassword",
   database: process.env.DB_NAME || "medusa_ecommerce",
   waitForConnections: true,
   connectionLimit: 10,
