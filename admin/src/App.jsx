a55748d27f878885816a807ff15edd39611abd7d
malithonline <120547828+malithonline@users.noreply.github.com>
2025-07-22 12:05:11 +0530

    fixed homepage,policy details

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -38,6 +38,10 @@ import NewPolicyDetailsSettings from "./components/NewPolicyDetailsSettings";
 import SignUpPage from "./components/SignUpPage";
 import HeaderFooterSettingsCreateOnly from "./components/HeaderFooterSettingsCreateOnly";
 
+import HomePageSettings from "./components/HomePageSettings"; //added import for HomePageSettings
+import PolicyDetailsSettings from "./components/PolicyDetailsSettings"; //added import for PolicyDetailsSettings
+
+
 const App = () => {
   const [loading, setLoading] = useState(true);
   const location = useLocation();
@@ -119,6 +123,13 @@ const App = () => {
               element={<CustomerManagedForm />}
             />
             <Route path="settings/about-us" element={<AboutUsSettings />} />
+
+            <Route path="settings/home-page" element={<HomePageSettings />} />
+
+            <Route
+              path="settings/policy-details"
+              element={<PolicyDetailsSettings />}
+            />
             <Route
               path="settings/header-footer"
               element={<HeaderFooterSettings />}
--- src/models/setting.model.js
+++ src/models/setting.model.js
@@ -7,11 +7,7 @@ const { getOrgMail } = require('../utils/organization');
 
 // Get header footer setting
 async function getHeaderFooterSetting() {
-  const orgMail = getOrgMail();
-  const [rows] = await pool.query(
-    "SELECT * FROM Header_Footer_Setting WHERE orgmail = ? LIMIT 1",
-    [orgMail]
-  );
+  const [rows] = await pool.query("SELECT * FROM Header_Footer_Setting LIMIT 1");
   return rows[0];
 }
 
@@ -23,7 +19,7 @@ async function updateHeaderFooterSetting(HeaderFooterSettingData) {
   if (existingSetting) {
     // Update existing header footer setting
     await pool.query(
-      "UPDATE Header_Footer_Setting SET Navbar_Logo_Url = ?, Footer_Copyright = ?, Nav_Icons = ?, Country_Blocks = ?, Footer_Links = ?, Social_Icons = ?, updated_at = CURRENT_TIMESTAMP WHERE idHeader_Footer_Setting = ? AND orgmail = ?",
+      "UPDATE Header_Footer_Setting SET Navbar_Logo_Url = ?, Footer_Copyright = ?, Nav_Icons = ?, Country_Blocks = ?, Footer_Links = ?, Social_Icons = ?, updated_at = CURRENT_TIMESTAMP WHERE idHeader_Footer_Setting = ?",
       [
         HeaderFooterSettingData.Navbar_Logo_Url,
         HeaderFooterSettingData.Footer_Copyright,
@@ -31,8 +27,7 @@ async function updateHeaderFooterSetting(HeaderFooterSettingData) {
         HeaderFooterSettingData.Country_Blocks,
         HeaderFooterSettingData.Footer_Links,
         HeaderFooterSettingData.Social_Icons,
-        existingSetting.idHeader_Footer_Setting,
-        orgMail
+        existingSetting.idHeader_Footer_Setting
       ]
     );
 
@@ -42,22 +37,21 @@ async function updateHeaderFooterSetting(HeaderFooterSettingData) {
   } else {
     // Create new header footer setting
     const result = await pool.query(
-      "INSERT INTO Header_Footer_Setting (Navbar_Logo_Url, Footer_Copyright, Nav_Icons, Country_Blocks, Footer_Links, Social_Icons, orgmail) VALUES (?,?,?,?,?,?,?)",
+      "INSERT INTO Header_Footer_Setting (Navbar_Logo_Url, Footer_Copyright, Nav_Icons, Country_Blocks, Footer_Links, Social_Icons) VALUES (?,?,?,?,?,?)",
       [
         HeaderFooterSettingData.Navbar_Logo_Url,
         HeaderFooterSettingData.Footer_Copyright,
         HeaderFooterSettingData.Nav_Icons,
         HeaderFooterSettingData.Country_Blocks,
         HeaderFooterSettingData.Footer_Links,
-        HeaderFooterSettingData.Social_Icons,
-        orgMail
+        HeaderFooterSettingData.Social_Icons
       ]
     );
 
     // Return newly created header footer setting
     const [rows] = await pool.query(
-      "SELECT * FROM Header_Footer_Setting WHERE idHeader_Footer_Setting = ? AND orgmail = ?",
-      [result[0].insertId, orgMail]
+      "SELECT * FROM Header_Footer_Setting WHERE idHeader_Footer_Setting = ?",
+      [result[0].insertId]
     );
     return rows[0];
   }
@@ -69,8 +63,7 @@ async function updateHeaderFooterSetting(HeaderFooterSettingData) {
 
 // Get about us setting
 async function getAboutUsSetting() {
-  const orgMail = getOrgMail();
-  const [rows] = await pool.query("SELECT * FROM About_Us_Setting WHERE orgmail = ? LIMIT 1", [orgMail]);
+  const [rows] = await pool.query("SELECT * FROM About_Us_Setting LIMIT 1");
   return rows[0];
 }
 
@@ -82,7 +75,7 @@ async function updateAboutUsSetting(AboutUsSettingData) {
   if (existingSetting) {
     // Update existing about us setting
     await pool.query(
-      "UPDATE About_Us_Setting SET Statistics = ?, Vision_Image_Url = ?, Vision_Title = ?, Vision_Description = ?, Mission_Image_Url = ?, Mission_Title = ?, Mission_Description = ?, Values_Image_Url = ?, Values_Title = ?, Values_Description = ?, Team_Image_Url = ?, Team_Title = ?, Team_Description = ?, Customer_Service_Image_Url = ?, Customer_Service_Title = ?, Customer_Service_Description = ?, Shopping_Experience_Image_Url = ?, Shopping_Experience_Title = ?, Shopping_Experience_Description = ?, Shopping_Experience_Button_Text = ?, updated_at = CURRENT_TIMESTAMP WHERE idAbout_Us_Setting = ? AND orgmail = ?",
+      "UPDATE About_Us_Setting SET Statistics = ?, Vision_Image_Url = ?, Vision_Title = ?, Vision_Description = ?, Mission_Image_Url = ?, Mission_Title = ?, Mission_Description = ?, Values_Image_Url = ?, Values_Title = ?, Values_Description = ?, Features = ?, Why_Choose_Us_Image_Url = ?, Shopping_Experience_Title = ?, Shopping_Experience_Description = ?, Shopping_Experience_Button_Text = ?, updated_at = CURRENT_TIMESTAMP WHERE idAbout_Us_Setting = ?",
       [
         AboutUsSettingData.Statistics,
         AboutUsSettingData.Vision_Image_Url,
@@ -94,18 +87,12 @@ async function updateAboutUsSetting(AboutUsSettingData) {
         AboutUsSettingData.Values_Image_Url,
         AboutUsSettingData.Values_Title,
         AboutUsSettingData.Values_Description,
-        AboutUsSettingData.Team_Image_Url,
-        AboutUsSettingData.Team_Title,
-        AboutUsSettingData.Team_Description,
-        AboutUsSettingData.Customer_Service_Image_Url,
-        AboutUsSettingData.Customer_Service_Title,
-        AboutUsSettingData.Customer_Service_Description,
-        AboutUsSettingData.Shopping_Experience_Image_Url,
+        AboutUsSettingData.Features,
+        AboutUsSettingData.Why_Choose_Us_Image_Url,
         AboutUsSettingData.Shopping_Experience_Title,
         AboutUsSettingData.Shopping_Experience_Description,
         AboutUsSettingData.Shopping_Experience_Button_Text,
-        existingSetting.idAbout_Us_Setting,
-        orgMail
+        existingSetting.idAbout_Us_Setting
       ]
     );
 
@@ -115,7 +102,7 @@ async function updateAboutUsSetting(AboutUsSettingData) {
   } else {
     // Create new about us setting
     const result = await pool.query(
-      "INSERT INTO About_Us_Setting (Statistics, Vision_Image_Url, Vision_Title, Vision_Description, Mission_Image_Url, Mission_Title, Mission_Description, Values_Image_Url, Values_Title, Values_Description, Team_Image_Url, Team_Title, Team_Description, Customer_Service_Image_Url, Customer_Service_Title, Customer_Service_Description, Shopping_Experience_Image_Url, Shopping_Experience_Title, Shopping_Experience_Description, Shopping_Experience_Button_Text, orgmail) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+      "INSERT INTO About_Us_Setting (Statistics, Vision_Image_Url, Vision_Title, Vision_Description, Mission_Image_Url, Mission_Title, Mission_Description, Values_Image_Url, Values_Title, Values_Description, Features, Why_Choose_Us_Image_Url, Shopping_Experience_Title, Shopping_Experience_Description, Shopping_Experience_Button_Text) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
       [
         AboutUsSettingData.Statistics,
         AboutUsSettingData.Vision_Image_Url,
@@ -127,24 +114,18 @@ async function updateAboutUsSetting(AboutUsSettingData) {
         AboutUsSettingData.Values_Image_Url,
         AboutUsSettingData.Values_Title,
         AboutUsSettingData.Values_Description,
-        AboutUsSettingData.Team_Image_Url,
-        AboutUsSettingData.Team_Title,
-        AboutUsSettingData.Team_Description,
-        AboutUsSettingData.Customer_Service_Image_Url,
-        AboutUsSettingData.Customer_Service_Title,
-        AboutUsSettingData.Customer_Service_Description,
-        AboutUsSettingData.Shopping_Experience_Image_Url,
+        AboutUsSettingData.Features,
+        AboutUsSettingData.Why_Choose_Us_Image_Url,
         AboutUsSettingData.Shopping_Experience_Title,
         AboutUsSettingData.Shopping_Experience_Description,
-        AboutUsSettingData.Shopping_Experience_Button_Text,
-        orgMail
+        AboutUsSettingData.Shopping_Experience_Button_Text
       ]
     );
 
     // Return newly created about us setting
     const [rows] = await pool.query(
-      "SELECT * FROM About_Us_Setting WHERE idAbout_Us_Setting = ? AND orgmail = ?",
-      [result[0].insertId, orgMail]
+      "SELECT * FROM About_Us_Setting WHERE idAbout_Us_Setting = ?",
+      [result[0].insertId]
     );
     return rows[0];
   }
@@ -156,30 +137,24 @@ async function updateAboutUsSetting(AboutUsSettingData) {
 
 // Fetch policy details setting
 async function getPolicyDetailsSetting() {
-  const orgMail = getOrgMail();
-  const [rows] = await pool.query(
-    "SELECT * FROM Policy_Details_Setting WHERE orgmail = ? LIMIT 1",
-    [orgMail]
-  );
+  const [rows] = await pool.query("SELECT * FROM Policy_Details_Setting LIMIT 1");
   return rows[0] || null;
 }
 
 // Update policy details setting
 async function updatePolicyDetailsSetting(PolicyDetailsSettingData) {
-  const orgMail = getOrgMail();
   const currentPolicyDetailsSetting = await getPolicyDetailsSetting();
 
   if (currentPolicyDetailsSetting) {
     // Update existing policy details setting
     await pool.query(
-      "UPDATE Policy_Details_Setting SET Legal_Policy_Content = ?,  Privacy_Policy_Content = ?, Security_Policy_Content = ?, Terms_Of_Service_Content = ?, updated_at = CURRENT_TIMESTAMP WHERE idPolicy_Details_Setting = ? AND orgmail = ?",
+      "UPDATE Policy_Details_Setting SET Legal_Policy_Content = ?,  Privacy_Policy_Content = ?, Security_Policy_Content = ?, Terms_Of_Service_Content = ?, updated_at = CURRENT_TIMESTAMP WHERE idPolicy_Details_Setting = ?",
       [
         PolicyDetailsSettingData.Legal_Policy_Content,
         PolicyDetailsSettingData.Privacy_Policy_Content,
         PolicyDetailsSettingData.Security_Policy_Content,
         PolicyDetailsSettingData.Terms_Of_Service_Content,
-        currentPolicyDetailsSetting.idPolicy_Details_Setting,
-        orgMail
+        currentPolicyDetailsSetting.idPolicy_Details_Setting
       ]
     );
 
@@ -189,20 +164,19 @@ async function updatePolicyDetailsSetting(PolicyDetailsSettingData) {
   } else {
     // Create new policy details setting
     const result = await pool.query(
-      "INSERT INTO Policy_Details_Setting (Legal_Policy_Content, Privacy_Policy_Content, Security_Policy_Content, Terms_Of_Service_Content, orgmail) VALUES (?,?,?,?,?)",
+      "INSERT INTO Policy_Details_Setting (Legal_Policy_Content, Privacy_Policy_Content, Security_Policy_Content, Terms_Of_Service_Content) VALUES (?,?,?,?)",
       [
         PolicyDetailsSettingData.Legal_Policy_Content,
         PolicyDetailsSettingData.Privacy_Policy_Content,
         PolicyDetailsSettingData.Security_Policy_Content,
-        PolicyDetailsSettingData.Terms_Of_Service_Content,
-        orgMail
+        PolicyDetailsSettingData.Terms_Of_Service_Content
       ]
     );
 
     // Return newly created policy details setting
     const [rows] = await pool.query(
-      "SELECT * FROM Policy_Details_Setting WHERE idPolicy_Details_Setting = ? AND orgmail = ?",
-      [result[0].insertId, orgMail]
+      "SELECT * FROM Policy_Details_Setting WHERE idPolicy_Details_Setting = ?",
+      [result[0].insertId]
     );
 
     return rows[0];
@@ -214,26 +188,23 @@ async function updatePolicyDetailsSetting(PolicyDetailsSettingData) {
 // -----------------------------------
 // Fetch home page setting
 async function getHomePageSetting() {
-  const orgMail = getOrgMail();
-  const [rows] = await pool.query("SELECT * FROM Home_Page_Setting WHERE orgmail = ? LIMIT 1", [orgMail]);
+  const [rows] = await pool.query("SELECT * FROM Home_Page_Setting LIMIT 1");
   return rows[0] || null;
 }
 
 // Update home page setting
 async function updateHomePageSetting(HomePageSettingData) {
-  const orgMail = getOrgMail();
   const currentHomePageSetting = await getHomePageSetting();
   if (currentHomePageSetting) {
     // Update existing home page setting
     await pool.query(
-      "UPDATE Home_Page_Setting SET Hero_Images = ?, Working_Section_Title = ?, Working_Section_Description = ?, Working_Items = ?, updated_at = CURRENT_TIMESTAMP WHERE idHome_Page_Setting = ? AND orgmail = ?",
+      "UPDATE Home_Page_Setting SET Hero_Images = ?, Working_Section_Title = ?, Working_Section_Description = ?, Working_Items = ?, updated_at = CURRENT_TIMESTAMP WHERE idHome_Page_Setting = ?",
       [
         HomePageSettingData.Hero_Images,
         HomePageSettingData.Working_Section_Title,
         HomePageSettingData.Working_Section_Description,
         HomePageSettingData.Working_Items,
-        currentHomePageSetting.idHome_Page_Setting,
-        orgMail
+        currentHomePageSetting.idHome_Page_Setting
       ]
     );
     // Return updated home page setting
@@ -242,19 +213,18 @@ async function updateHomePageSetting(HomePageSettingData) {
   } else {
     // Create new home page setting
     const result = await pool.query(
-      "INSERT INTO Home_Page_Setting (Hero_Images, Working_Section_Title, Working_Section_Description, Working_Items, orgmail) VALUES (?,?,?,?,?)",
+      "INSERT INTO Home_Page_Setting (Hero_Images, Working_Section_Title, Working_Section_Description, Working_Items) VALUES (?,?,?,?)",
       [
         HomePageSettingData.Hero_Images,
         HomePageSettingData.Working_Section_Title,
         HomePageSettingData.Working_Section_Description,
-        HomePageSettingData.Working_Items,
-        orgMail
+        HomePageSettingData.Working_Items
       ]
     );
     // Return newly created home page setting
     const [rows] = await pool.query(
-      "SELECT * FROM Home_Page_Setting WHERE idHome_Page_Setting = ? AND orgmail = ?",
-      [result[0].insertId, orgMail]
+      "SELECT * FROM Home_Page_Setting WHERE idHome_Page_Setting = ?",
+      [result[0].insertId]
     );
     return rows[0];
   }
